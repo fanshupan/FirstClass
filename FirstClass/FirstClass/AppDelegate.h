4982bc842dd66a2dadad1e17442ebf3ef53722e0
fanshupan <user1@Wus-MacBook-Pro.local>
2014-08-30 13:41:16 +0800

    1:注释

---
+++ FirstClass/FirstClass/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FirstClass
+//
+//  Created by user1 on 14-8-30.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
